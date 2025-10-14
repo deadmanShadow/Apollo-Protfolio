0d3fcfe2730a7cf043e8643bb7686aaa2cc3a936
deadmanShadow <raihanshamil33@gmail.com>
2025-10-14 20:59:20 +0600

    lib modified with adding error pages fixing 404 problems

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,37 @@
+export const publicNavbarItems = [
+  {
+    title: "About Me",
+    url: "/",
+  },
+  {
+    title: "Projects",
+    url: "/projects",
+  },
+  {
+    title: "Blogs",
+    url: "/blogs",
+  },
+];
+
+export const dashboardSidebarItems = [
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+  {
+    title: "Blogs",
+    url: "/dashboard/blogs",
+  },
+  {
+    title: "Projects",
+    url: "/dashboard/projects",
+  },
+  {
+    title: "Experiences",
+    url: "/dashboard/experiences",
+  },
+  {
+    title: "Profile Settings",
+    url: "/dashboard/profile",
+  },
+];
